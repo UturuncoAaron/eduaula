ec80ea654770af29e4d54bb513b6cd40ec39e639
Aarón <uturuncoaaron@gmail.com>
2026-06-06 16:41:35 -0500

    fix  ultimando pruebas v5

--- src/environments/environment.local.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://192.168.18.8:3000/api'
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tuCOLEGIO.edu.pe/api'
+    apiUrl: 'https://backendcapstone-five.vercel.app/api'
 };
